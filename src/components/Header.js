160daf148a473d89a151550df981b51ac017f915
jay suthar <jay37192@gmail.com>
2024-05-21 11:45:39 +0530

    #

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,47 +1,3 @@
-// "use client";
-// import React from "react";
-// import sanityClient, { fetchDataFromSanity } from "../client";
-
-// const data = await fetchDataFromSanity();
-// const querylink = `*[_type == 'headerlink']{
-//   _id,
-//   name,
-//    link,
-// }`;
-
-// const datalink = await sanityClient.fetch(querylink);
-
-// const Header = () => {
-//   return (
-//     <div className="bg-[#04152d] container">
-//       {data.map((list) => {
-//         return (
-//           <div className="flex justify-between px-10 py-6" key={list._id}>
-//             <div>
-//               <h1 className="font-semibold text-3xl text-white cursor-pointer hover:text-[#576cbc] duration-500">
-//                 {list.label}
-//               </h1>
-//             </div>
-//             <div className="flex gap-8">
-//               {datalink.map((item) => (
-//                 <a
-//                   href={item.link}
-//                   className="text-[25px] text-white cursor-pointer hover:text-[#576cbc] duration-500"
-//                   key={item._id}
-//                 >
-//                   {item.name}
-//                 </a>
-//               ))}
-//             </div>
-//           </div>
-//         );
-//       })}
-//     </div>
-//   );
-// };
-
-// export default Header;
-
 "use client";
 import React, { useState } from "react";
 import sanityClient, { fetchDataFromSanity } from "../client";
@@ -70,7 +26,7 @@ const Header = () => {
           <div className="md:hidden">
             <button
               onClick={() => setMenuOpen(!menuOpen)}
-              className="text-white focus:outline-none"
+              className="text-gray-700 focus:outline-none"
             >
               {menuOpen ? (
                 <svg
